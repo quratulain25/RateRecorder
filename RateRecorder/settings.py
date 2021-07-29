55bcad4871332e3d4c0336b0e97643ef05e5f2dc
quratulain25 <quratulain@arbisoft.com>
2021-07-30 02:22:33 +0500

    STATIC_ROOT settings updated.

--- RateRecorder/settings.py
+++ RateRecorder/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-95*eyqd8f7-d^z1j0dxp6n)pjdfbx&n=3=7s2@hodw+4irl#&c'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-95*eyqd8f7-d^z1j0dxp6n)pjdfbx&n=3=7s2@hodw+4irl#&c'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['raterecorder.herokuapp.com']
+ALLOWED_HOSTS = ['raterecorder.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -124,7 +124,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles') # added for heroku
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
